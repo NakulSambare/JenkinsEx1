67084c42b65fc4c4792e200fb69eb95913690dc7
Nakul Sambare <sambarenakul@gmail.com>
2024-01-21 14:53:08 +0530

    context path changed

--- src/main/java/com/jenkinsEx/JenkinsEx1/controller/SchoolController.java
+++ src/main/java/com/jenkinsEx/JenkinsEx1/controller/SchoolController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/school")
+@RequestMapping("/JenkinsEx1/school")
 public class SchoolController {
 
     @Autowired
